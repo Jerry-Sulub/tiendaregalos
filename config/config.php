9d676e3ffa1a0b905df508307bffe7792a2ea918
Jerry-Sulub <jerrysulub2104@gmail.com>
2022-04-29 11:13:36 -0500

    Primer commit

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+	/**
+	 * 
+	 * Configuraciones para el sistema
+	 * 
+	*/
+
+	//Nombre del controlador por defecto
+	define("CONTROLADOR_PRINCIPAL", "Bienvenida");
+
+	//Nombre del metodo por defecto
+	define("ACCION_PRINCIPAL", "index");
---
+++ config/database.php
@@ -0,0 +1,18 @@
+<?php
+	
+	/**
+	 * 
+	 * Conexion a la base de datos
+	 * 
+	*/
+
+	class Conectar {
+		
+		public static function conexion(){
+			
+			$conexion = new mysqli("host", "user", "pass", "dbname");
+			return $conexion;
+			
+		}
+	}
+?>
