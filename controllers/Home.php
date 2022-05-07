dc2a238045a57ce61519e46be57713a86a04aa54
AleValladares <alex.uc2101@hotmail.com>
2022-05-06 23:02:54 -0500

    .

--- controllers/Home.php
+++ controllers/Home.php
@@ -4,14 +4,21 @@ class HomeController {
 
     public function __construct()
     {
-        //Agregar el archivo de modelo
+        require_once "models/RegalosModel.php";
     }
 
     public function index()
     {
+			$regalos = new tiendaRegalos_model();
+			$data["regalos"] = $regalos->get_regalos();
+			require_once "views/public/eliminar.php";
+    }
 
-        require_once "views/public/home.php";
-
+    public function eliminar($id){
+        $regalos = new tiendaRegalos_model();
+        $regalos->eliminar($id);
+        $this->index();
     }
 
 }
+?>
--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
-require_once "config/config.php";
+	require_once "config/config.php";
 	require_once "config/routes.php";
 	require_once "config/database.php";
 	require_once "controllers/Home.php";
-	
+
 	if(isset($_GET['c'])){
-		
+
 		$controlador = cargarControlador($_GET['c']);
-		
+
 		if(isset($_GET['a'])){
 			if(isset($_GET['id'])){
 				cargarAccion($controlador, $_GET['a'], $_GET['id']);
@@ -18,9 +18,9 @@ require_once "config/config.php";
 			} else {
 			cargarAccion($controlador, ACCION_PRINCIPAL);
 		}
-		
+
 		} else {
-		
+
 		$controlador = cargarControlador(CONTROLADOR_PRINCIPAL);
 		$accionTmp = ACCION_PRINCIPAL;
 		$controlador->$accionTmp();
