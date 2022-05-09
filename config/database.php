f760403b561fea1d870ffabf857356f018e40792
EnriqueCocom <le18080485@merida.tecnm.mx>
2022-05-09 10:28:04 -0500

    6

--- config/database.php
+++ config/database.php
@@ -1,21 +1,22 @@
 <?php
-	
-	/**
-	 * 
-	 * Conexion a la base de datos
-	 * 
-	*/
 
-	class Conectar {
-	
-		public static function conexion(){
-			$conexion = new mysqli(
-				"localhost", 
-				"root", 
-				"", 
-				"tiendaregalos"
-			);
-			return $conexion;
-		}
+/**
+ * 
+ * Conexion a la base de datos
+ * 
+ */
+
+class Conectar
+{
+
+	public static function conexion()
+	{
+		$conexion = new mysqli(
+			"localhost",
+			"root",
+			"",
+			"tiendaregalos"
+		);
+		return $conexion;
 	}
-?>
+}
