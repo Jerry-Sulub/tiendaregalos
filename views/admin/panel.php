2e1dae21f514b84586f5ed762d5c35a4e7e5344e
Angelin9908 <angelpiste05@gmail.com>
2022-05-10 17:00:03 -0500

    Session

--- views/admin/panel.php
+++ views/admin/panel.php
@@ -1,37 +1,37 @@
+<?php
 
-    <?php
-           session_start();
-           if(!isset($_SESSION['auth']))
-           {
-               header('location: index.php?c=secure');
-           }
-           include "template/header.php";
-    ?>
-    <div class="ancho">
-        <table class="tablaPanel">
-            <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Descripción</th>
-                <th>Piezas</th>
-                <th>Precio</th>
-                <th>Tipo</th>
-                <th>Imagen</th>
-            </tr>
-            <?php
-                foreach($data["productos"] as $dato){
-                echo "<tr>";
-                echo "<td>".$dato["ID"]."</td>";
-                echo "<td>".$dato["nombre"]."</td>";
-                echo "<td>".$dato["descripcion"]."</td>";
-                echo "<td>".$dato["piezas"]."</td>";
-                echo "<td>".$dato["precio"]."</td>";
-                echo "<td>".$dato["tipo"]."</td>";
-                echo "<td><img src='".$dato["img"]."' alt='Foto del producto'> </td>";
-                echo "</tr>";     
-            }
+session_start();
+if (!isset($_SESSION['auth'])) {
+    header('location: index.php?c=secure');
+}
+include "template/header.php";
+?>
+<div class="ancho">
+    <table class="tablaPanel">
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Descripción</th>
+            <th>Piezas</th>
+            <th>Precio</th>
+            <th>Tipo</th>
+            <th>Imagen</th>
+        </tr>
+        <?php
+        foreach ($data["productos"] as $dato) {
+            echo "<tr>";
+            echo "<td>" . $dato["ID"] . "</td>";
+            echo "<td>" . $dato["nombre"] . "</td>";
+            echo "<td>" . $dato["descripcion"] . "</td>";
+            echo "<td>" . $dato["piezas"] . "</td>";
+            echo "<td>" . $dato["precio"] . "</td>";
+            echo "<td>" . $dato["tipo"] . "</td>";
+            echo "<td><img src='" . $dato["img"] . "' alt='Foto del producto'> </td>";
+            echo "</tr>";
+        }
         ?>
-        </table>
-        
-</body>
-</html>
+    </table>
+
+    </body>
+
+    </html>
