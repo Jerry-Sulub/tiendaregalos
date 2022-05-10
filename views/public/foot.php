ff780816ae4d4ad43a43ea3c4f4be9de7fac1360
Angelin9908 <angelpiste05@gmail.com>
2022-05-10 10:23:27 -0500

    foot2

--- views/public/foot.php
+++ views/public/foot.php
@@ -1,18 +1,20 @@
 
 <div class="foot">
   <div class="ancho">
-     <div class="Contactos">
+     <div class="contactos">
         <h3>Puedes encontrarnos en:</h3>
-        <a href="https://www.facebook.com/Detalles.Anadru.Izamal"><img src="assets/images/icon_face.png" alt="">Detalles Anadru</a>
+        <a href="https://www.facebook.com/Detalles.Anadru.Izamal"><img src="assets/images/icon_face.png" alt=""> Detalles Anadru</a>
         <p>o</p>
-        <a href=""><img src="assets/images/icon_face.png" alt="">Anadru</a>
-        <p><img src="assets/images/icon_telf.png" alt=""> 999 439 5942</p>
+        <a href=""><img src="assets/images/icon_face.png" alt=""> Anadru</a>
     </div>
     <div class="contactos">
-      
+    <div class="logo">
+      <a href="../admin/login.php"><img src="assets/images/logo_anadru.png" alt=""></a>
+   
     </div>
-    <div class="Contactos">
-      
+    </div>
+    <div class="contactos">
+    <p><img src="assets/images/icon_telf.png" alt=""> 999 439 5942</p>
     </div>
     <div class="copy">
       <p>Copyright 2022 &copy;</p>
