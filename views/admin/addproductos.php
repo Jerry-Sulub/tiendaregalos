f2654790fa60613d7c144d23460cd201a7ca3748
Jerry-Sulub <sulub.c.gerardo@gmail.com>
2022-05-10 15:13:55 -0500

    Estilos

--- views/admin/addproductos.php
+++ views/admin/addproductos.php
@@ -12,22 +12,23 @@ include "template/header.php";
         <form action="index.php?c=home&a=guardar" class="text-center" enctype="multipart/form-data" method="post" name="producto">
 
             <div class="form-group">
+                <br>
                 <label for="name">Nombre</label>
                 <input type="text" class="text-center" id="name" name="nombre">
-                <br>
+                <br><br>
                 <label for="comment">Descripción del producto</label id="com">
                 <br>
                 <textarea name="descripcion" id="comment" cols="60" rows="10" resizable="false"></textarea>
-                <br>
+                <br><br>
                 <label for="piezas" id="ape">Piezas</label>
                 <input type="text" class="text-center" name="piezas" id="piezas">
-                <br>
+                <br><br>
                 <label for="precio" id="ape">Precio</label>
                 <input type="text" class="text-center" name="precio" id="precio">
-                <br>
+                <br><br>
                 <label for="tipo" id="ape">Tipo</label>
                 <input type="text" class="text-center" name="tipo" id="tipo">
-            </div>
+            </div><br>
             <div class="form-group">
                 <label for="im" id="im">Imagen</label>
                 <input type="file" name="imagen" id="im">
