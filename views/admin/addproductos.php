2f645b07c87e92cbb3d073ed9807e7a1069cdd34
Jerry-Sulub <sulub.c.gerardo@gmail.com>
2022-05-10 15:08:36 -0500

    Styles

--- views/admin/addproductos.php
+++ views/admin/addproductos.php
@@ -1,49 +1,50 @@
 <?php
 
-    session_start();
-    if(!isset($_SESSION['auth']))
-    {
-        header('location: index.php?c=secure');
-    }
-    include "template/header.php";
+session_start();
+if (!isset($_SESSION['auth'])) {
+    header('location: index.php?c=secure');
+}
+include "template/header.php";
 ?>
+<br><br>
+<div class="ancho">
+    <div class="agregar">
+        <form action="index.php?c=home&a=guardar" class="text-center" enctype="multipart/form-data" method="post" name="producto">
 
-    <form action="index.php?c=home&a=guardar" class="text-center" enctype="multipart/form-data" method="post"
-        name="producto">
-        
-        <div class="form-group">
-            <label for="name">Nombre</label>
-            <input type="text" class="text-center" id="name" name="nombre">
-            <br>
-            <label for="comment">Descripción del producto</label id="com">
-            <br>
-            <textarea name="descripcion" id="comment" cols="60" rows="10" resizable="false"></textarea>
-            <br>
-            <label for="piezas" id="ape">Piezas</label>
-            <input type="text" class="text-center" name="piezas" id="piezas">
-            <br>
-            <label for="precio" id="ape">Precio</label>
-            <input type="text" class="text-center" name="precio" id="precio">
-            <br>
-            <label for="tipo" id="ape">Tipo</label>
-            <input type="text" class="text-center" name="tipo" id="tipo">
-        </div>        
-        <div class="form-group">
-            <label for="im" id="im">Imagen</label>
-            <input type="file" name="imagen" id="im">
-        </div>
-        <a id="btn" style="background-color: red; padding: 10px; margin-top: 20px; display: block; width: 50px; border-radius: 10px; color: white;">
-            Enviar
-        </a>
-    </form>
+            <div class="form-group">
+                <label for="name">Nombre</label>
+                <input type="text" class="text-center" id="name" name="nombre">
+                <br>
+                <label for="comment">Descripción del producto</label id="com">
+                <br>
+                <textarea name="descripcion" id="comment" cols="60" rows="10" resizable="false"></textarea>
+                <br>
+                <label for="piezas" id="ape">Piezas</label>
+                <input type="text" class="text-center" name="piezas" id="piezas">
+                <br>
+                <label for="precio" id="ape">Precio</label>
+                <input type="text" class="text-center" name="precio" id="precio">
+                <br>
+                <label for="tipo" id="ape">Tipo</label>
+                <input type="text" class="text-center" name="tipo" id="tipo">
+            </div>
+            <div class="form-group">
+                <label for="im" id="im">Imagen</label>
+                <input type="file" name="imagen" id="im">
+            </div>
+            <a id="btn" style="background-color: red; padding: 10px; margin-top: 20px; display: block; width: 50px; border-radius: 10px; color: white;">
+                Enviar
+            </a>
+        </form>
+    </div>
+</div>
+<script>
+    let a = document.getElementById('btn');
 
-    <script>
-        let a = document.getElementById('btn');
-
-        a.addEventListener('click', event=>{
-            document.producto.submit();
-        });
-    </script>
+    a.addEventListener('click', event => {
+        document.producto.submit();
+    });
+</script>
 </body>
 
 </html>
